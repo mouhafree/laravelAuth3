b7a3507ba75b72a2fb480e7b95c3847d2b67f477
OumyMbaye <oumish97mbaye@gmail.com>
2023-09-20 11:00:56 -0900

    Laravel Auth

---
+++ resources/views/inscription.blade.php
@@ -0,0 +1,35 @@
+
+    @extends('layouts.app');
+
+    @section('content')
+
+
+    @section('section2')
+
+    orem ipsum, dolor sit amet consectetur adipisicing elit.
+     Animi odit atque pariatur consectetur cupiditate alias qui, earum eum perferendis
+    , velit corrupti beatae voluptatem, vitae rerum? Sit ab eveniet explicabo nobis?
+    @endsection
+
+    <form action="">
+      <input type="text">
+      <input type="text">
+    </form>
+    @endsection
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                Accueil
+                </div>
+                <div class="col">
+                Ajouter
+                </div>
+                <div class="col">
+                Lister
+                </div>
+            </div>
+        </div>
+        <h6>Inscription</h6>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/lister.blade.php
@@ -0,0 +1,1 @@
+@include('layouts.nav_bar');
